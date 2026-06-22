8e0422df06da17596999ef3ac9462d290c094b13
criskell <cris.moraes.ds@gmail.com>
2026-06-22 05:16:29 -0300

    feat(boot): jump to rust

--- Makefile
+++ Makefile
@@ -3,30 +3,32 @@ kernel := output/$(arch)/disk/boot/kernel.bin
 ld_script := src/arch/$(arch)/linker.ld
 output := output/$(arch)
 disk := $(output)/disk
+kernel_library := target/$(arch)/debug/libark.a
 
 assembly_files := $(wildcard src/arch/$(arch)/*.S)
 object_files := $(patsubst src/arch/$(arch)/%.S, output/$(arch)/%.o, $(assembly_files))
 
-.PHONY: all build clean run
+.PHONY: all build clean run kernel
 
 all: run
 
-kernel: $(kernel)
+kernel:
+	cargo build
 
 run: build
 	qemu-system-x86_64 -cdrom output/$(arch)/ark.iso
 
 build: $(kernel)
 	@mkdir -p $(disk)/boot/grub
-	@cp grub.cfg $(disk)/boot/grub
+	@cp grub.cfg $(disk)/boot/grub	
 	@grub-mkrescue -o output/$(arch)/ark.iso $(disk)
 
 clean:
 	@rm -rf output
 
-$(kernel): $(object_files) $(ld_script)
+$(kernel): kernel $(object_files) $(ld_script)
 	@mkdir -p $(shell dirname $(kernel))
-	ld -n -T $(ld_script) -o $(kernel) $(object_files)
+	ld -n -T $(ld_script) -o $(kernel) --gc-sections $(object_files) $(kernel_library)
 
 output/$(arch)/%.o: src/arch/$(arch)/%.S 
 	@mkdir -p $(shell dirname $@)
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,69 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-#![feature(abi_x86_interrupt)]
 
-use crate::qemu::QemuExitCode;
-pub mod arch;
-pub mod memory;
-#[macro_use]
-pub mod screen;
-pub mod pci;
-pub mod qemu;
-pub mod serial;
-pub mod text;
+use core::panic::{self, PanicInfo};
 
-pub trait Testable {
-    fn run(&self);
-}
+#[no_mangle]
+pub extern "C" fn kernel_enter() {
+    let message = b"Hello WorlZ!";
+    let color_byte = 0x1f;
 
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_print!("[ok]");
-    }
-}
+    let mut hello_colored = [color_byte; 24];
 
-pub fn test_runner(tests: &[&dyn Testable]) {
-    println!("Running {} tests", tests.len());
-
-    for test in tests {
-        test.run();
+    for (i, char_byte) in message.into_iter().enumerate() {
+        hello_colored[i * 2] = *char_byte;
     }
 
-    qemu::exit(QemuExitCode::Success);
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+
+    unsafe {
+        *buffer_ptr = hello_colored;
+    }
+
+    loop {}
 }
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// #![feature(custom_test_frameworks)]
+// #![test_runner(crate::test_runner)]
+// #![reexport_test_harness_main = "test_main"]
+// #![feature(abi_x86_interrupt)]
+
+// use crate::qemu::QemuExitCode;
+// pub mod arch;
+// pub mod memory;
+// #[macro_use]
+// pub mod screen;
+// pub mod pci;
+// pub mod qemu;
+// pub mod serial;
+// pub mod text;
+
+// pub trait Testable {
+//     fn run(&self);
+// }
+
+// impl<T> Testable for T
+// where
+//     T: Fn(),
+// {
+//     fn run(&self) {
+//         serial_print!("{}...\t", core::any::type_name::<T>());
+//         self();
+//         serial_print!("[ok]");
+//     }
+// }
+
+// pub fn test_runner(tests: &[&dyn Testable]) {
+//     println!("Running {} tests", tests.len());
+
+//     for test in tests {
+//         test.run();
+//     }
+
+//     qemu::exit(QemuExitCode::Success);
+// }
